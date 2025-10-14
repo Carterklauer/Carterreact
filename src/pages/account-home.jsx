75097dcdcbbc38a773a5b7fc5fa4f0ee398b53ae
Carter Klauer <45862952+Carterklauer@users.noreply.github.com>
2025-10-13 19:20:44 -0500

    Update account-home.jsx

--- src/pages/account-home.jsx
+++ src/pages/account-home.jsx
@@ -1,2 +1,26 @@
-import * as React from "/react"
-export default function AccountHome(){}
+import * as React from "react"
+export default function AccountHome(){
+  const [User, SetUser]=React.useState({});
+  return(
+    <>
+      <header><h1 className="admin">Account Home</h1></header>
+      <main>
+        <ul className="menu">
+          <li className="menu"><button onClick={()=>location.assign("/get-users")}>Get Users</button></li>
+          <li className="menu"><button onClick={()=>location.assign("/change-password")}>Change Password</button></li>
+          <li className="menu"><button onClick={()=>location.assign("/administrator")}>Administrator</button>
+      </li>
+    </ul>
+    <ul className="menu">
+      <li className="menu">
+        <button>
+          <a className="menu" href="/friends-content">
+            Friend Content
+          </a>
+        </button>
+      </li>
+    </ul>
+  </main>
+</>
+  )
+}
