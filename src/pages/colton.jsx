ddf32e0ad8ebdee6ffd8f2059eb278823bb6e6f9
Carter Klauer <45862952+Carterklauer@users.noreply.github.com>
2026-01-08 17:32:51 -0600

    Update colton.jsx

--- src/pages/colton.jsx
+++ src/pages/colton.jsx
@@ -35,8 +35,17 @@ export default function Colton(){
       <div className="close-button" onClick={()=>ClosePopup()}>&times;</div>
     </Popup>
     <img className="portrait" src="https://cdn.glitch.com/1760db9d-c13a-4b0a-a542-608419c30c2b%2F65756298_444546549731261_1786822092965019648_n.jpg?v=1602466478548" border={0} alt="Colton playing Baseball 2." onClick={()=>OpenPopup("pic2")}/>
-    <img className="portrait" src="https://cdn.glitch.com/1760db9d-c13a-4b0a-a542-608419c30c2b%2F65977661_649244112259034_4839865523351060480_n.jpg?v=1602466482409" border={0} alt="Colton playing baseball 3."/>
+    <Popup visible={CheckID("pic2")} onClose={false} style={{width:500, height:800}} id="pic2">
+      <div className="close-button" onClick={()=>ClosePopup()}>&times;</div>
+    </Popup>
+    <img className="portrait" src="https://cdn.glitch.com/1760db9d-c13a-4b0a-a542-608419c30c2b%2F65977661_649244112259034_4839865523351060480_n.jpg?v=1602466482409" border={0} alt="Colton playing baseball 3." onClick={()=>OpenPopup("pic3")}/>
+    <Popup visible={CheckID("pic3")} onClose={false} style={{width:500, height:800}} id="pic3">
+      <div className="close-button" onClick={()=>ClosePopup()}>&times;</div>
+    </Popup>
     <img className="landscape" src="https://cdn.glitch.com/1760db9d-c13a-4b0a-a542-608419c30c2b%2F71003865_533970900687649_3178975934543822848_n.jpg?v=1602466494940" border={0} alt="Colton Playing baseball 4."/>
+    <Popup visible={CheckID("pic4")} onClose={false} style={{width:500, height:800}} id="pic4">
+      <div className="close-button" onClick={()=>ClosePopup()}>&times;</div>
+    </Popup>
     <img className="portrait" src="https://cdn.glitch.com/1760db9d-c13a-4b0a-a542-608419c30c2b%2F71237568_2540484516038687_3505501347208232960_n.jpg?v=1602466502219" border={0} alt="Colton Playing baseball 5."/>
     <img className="portrait" src="https://cdn.glitch.com/1760db9d-c13a-4b0a-a542-608419c30c2b%2F71325705_474807153103911_5717455254639869952_n.jpg?v=1602466506909" border={0} alt="Colton playing Baseball 6."/>
     <img className="portrait" src="https://cdn.glitch.com/1760db9d-c13a-4b0a-a542-608419c30c2b%2F72151848_244496169816310_1474778309295341568_n.jpg?v=1602466515582" border={0} alt="colton playing Baseball 7."/>
