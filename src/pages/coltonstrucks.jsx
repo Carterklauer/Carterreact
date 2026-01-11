de4e784306acf0da889f076fdda4f51984a16688
Carter Klauer <45862952+Carterklauer@users.noreply.github.com>
2026-01-10 21:36:23 -0600

    Create coltonstrucks.jsx

---
+++ src/pages/coltonstrucks.jsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+import {useEffect} from "react"
+
+export default ColtonsTrucks(){
+  return(
+    <>
+      <header><h1>Colton's Nissan Trucks</h1></header>
+      <p>Colton is a proud Nissan Trucks owner. His previous trucks were a 2005 Nissan Titan, which I own now, and 2019 Nissan Frontier. His current truck is 2021 Nissan Titan. His trucks have always been lifted with very aggressive mud tires.</p>
+    </>
+  )
+}
